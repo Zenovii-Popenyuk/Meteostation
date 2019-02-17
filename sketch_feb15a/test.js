6ef8ac5223f0d69a12ba048b200682c772d1970e
zenovii.popenyuk <35252560+Zenovii-Popenyuk@users.noreply.github.com>
2019-02-17 14:58:40 +0200

    Delete test.js

--- sketch_feb15a/test.js
+++
@@ -1,7 +0,0 @@
-<SCRIPT>
-function graphData(clicked_id) {var temp_but = document.getElementsByTagName('temp');
-if (clicked_id === 'temp1'){dps = tempData}else if (clicked_id === 'humm'){dps = hummData}else if (clicked_id === 'press'){dps = pressData}else if (clicked_id === 'temp2'){dps = temp2Data}}\n";
-var hh;
-var mm;
-)";
-zrhrdh
